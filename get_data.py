5cfdb4ff361d6b301a25b87584aaee5b50d8e366
unknown <kmeanskyle@gmail.com>
2019-10-15 16:21:46 -0800

    subset to Nome cells and download all years in one file

--- get_data.py
+++ get_data.py
@@ -1,13 +1,30 @@
+# Download ERA5 data for four grid cells around Nome using SLURM
+# No need for multiprocessing, as file sizes are small enough to download in one file 
+#   (each year = ~195kb)
 import cdsapi
 
 c = cdsapi.Client()
-
 c.retrieve(
     'reanalysis-era5-single-levels',
     {
         'product_type':'reanalysis',
         'variable':'snowfall',
-        'year':'2018',
+        'year':[
+            '1979','1980','1981',
+            '1982','1983','1984',
+            '1985','1986','1987',
+            '1988','1989','1990',
+            '1991','1992','1993',
+            '1994','1995','1996',
+            '1997','1998','1999',
+            '2000','2001','2002',
+            '2003','2004','2005',
+            '2006','2007','2008',
+            '2009','2010','2011',
+            '2012','2013','2014',
+            '2015','2016','2017',
+            '2018'
+        ],
         'month':[
             '01','02','03',
             '04','05','06',
@@ -37,6 +54,7 @@ c.retrieve(
             '18:00','19:00','20:00',
             '21:00','22:00','23:00'
         ],
+        'area':[64.75, 194.50, 64.25, 195],
         'format':'netcdf'
     },
-    '/workspace/UA/kmredilla/Nome_Mets/data/test_download_2018.nc')
+    '/workspace/UA/kmredilla/Nome_Mets/data/ERA5_snowfall_Nome_quad_1979-2018.nc')
