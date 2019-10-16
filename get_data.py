7fa99708346ecd91eb55800733fc429a42853150
unknown <kmeanskyle@gmail.com>
2019-10-15 16:27:11 -0800

    add query for snow depth

--- get_data.py
+++ get_data.py
@@ -1,9 +1,10 @@
-# Download ERA5 data for four grid cells around Nome using SLURM
+# Download ERA5 data snowfall and snow depth for four grid cells around Nome using SLURM
 # No need for multiprocessing, as file sizes are small enough to download in one file 
 #   (each year = ~195kb)
 import cdsapi
 
 c = cdsapi.Client()
+# get snowfall
 c.retrieve(
     'reanalysis-era5-single-levels',
     {
@@ -58,3 +59,59 @@ c.retrieve(
         'format':'netcdf'
     },
     '/workspace/UA/kmredilla/Nome_Mets/data/ERA5_snowfall_Nome_quad_1979-2018.nc')
+
+# get snow depth
+c.retrieve(
+    'reanalysis-era5-single-levels',
+    {
+        'product_type':'reanalysis',
+        'variable':'snow_depth',
+        'year':[
+            '1979','1980','1981',
+            '1982','1983','1984',
+            '1985','1986','1987',
+            '1988','1989','1990',
+            '1991','1992','1993',
+            '1994','1995','1996',
+            '1997','1998','1999',
+            '2000','2001','2002',
+            '2003','2004','2005',
+            '2006','2007','2008',
+            '2009','2010','2011',
+            '2012','2013','2014',
+            '2015','2016','2017',
+            '2018'
+        ],
+        'month':[
+            '01','02','03',
+            '04','05','06',
+            '07','08','09',
+            '10','11','12'
+        ],
+        'day':[
+            '01','02','03',
+            '04','05','06',
+            '07','08','09',
+            '10','11','12',
+            '13','14','15',
+            '16','17','18',
+            '19','20','21',
+            '22','23','24',
+            '25','26','27',
+            '28','29','30',
+            '31'
+        ],
+        'time':[
+            '00:00','01:00','02:00',
+            '03:00','04:00','05:00',
+            '06:00','07:00','08:00',
+            '09:00','10:00','11:00',
+            '12:00','13:00','14:00',
+            '15:00','16:00','17:00',
+            '18:00','19:00','20:00',
+            '21:00','22:00','23:00'
+        ],
+        'area':[64.75, 194.50, 64.25, 195],
+        'format':'netcdf'
+    },
+    '/workspace/UA/kmredilla/Nome_Mets/data/ERA5_snow_depth_Nome_quad_1979-2018.nc')
