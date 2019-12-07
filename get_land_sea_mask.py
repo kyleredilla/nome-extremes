bd4589e8dc32b4a3d8c9badbfd71bf5e04932f45
unknown <kmeanskyle@gmail.com>
2019-12-07 14:53:42 -0900

    use correct bounding box for landmask

--- get_land_sea_mask.py
+++ get_land_sea_mask.py
@@ -13,16 +13,12 @@ def get_data():
             'month':'01',
             'day':'01',
             'time':'01',
-            'area':[64.5, 194.5, 64, 195],
+            'area':[65, 194, 64, 195],
             'format':'netcdf'
         },
         '/workspace/UA/kmredilla/Nome_Mets/data/ERA5_land_sea_mask_Nome_sector.nc')
 
 if __name__ == '__main__':
     import copy
-    from multiprocessing import Pool
-    # multiprocessing run
-    pool = Pool()
-    # get all data
-    pool.map(get_data)
+    get_data()
     
