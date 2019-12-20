777ad0fc1c1e61ea68b0d0f98a158d162f2ae4af
unknown <kmeanskyle@gmail.com>
2019-12-19 17:01:24 -0900

    condense adj wrf output into single files

--- qmap_gcm_sf.R
+++ qmap_gcm_sf.R
@@ -13,26 +13,28 @@ wrf_sf <- readRDS("../Nome_Mets_aux/data/nome_gcm_sf.Rds")
 source("helpers.R")
 
 # perform quantile mapping
+# list to store results
+sf_lst <- list()
 # CM3
 cm3h_adj_lst <- qMap(
   era5_sf_adj$sf_adj, wrf_sf[[1]]$sf, 
   ret.deltas = TRUE
 )
-cm3f_adj <- qMap(
+sf_lst$cm3h <- cm3h_adj_lst$df
+sf_lst$cm3f <- qMap(
   sim = wrf_sf[[2]]$sf, 
   use.deltas = cm3h_adj_lst$deltas
 )
-cm3h_adj <- cm3h_adj_lst$df
 # lists for ecdf creation
 cm3h_ecdf_lst <- list(
   obs = era5_sf_adj$sf_adj,
-  sim = cm3h_adj$sim, 
-  sim_adj = cm3h_adj$sim_adj
+  sim = sf_lst$cm3h$sim, 
+  sim_adj = sf_lst$cm3h$sim_adj
 )
 cm3f_ecdf_lst <- list(
-  obs = cm3h_adj$sim_adj,
-  sim = cm3f_adj$sim, 
-  sim_adj = cm3f_adj$sim_adj
+  obs = sf_lst$cm3h$sim_adj,
+  sim = sf_lst$cm3f$sim, 
+  sim_adj = sf_lst$cm3f$sim_adj
 )
 
 # CCSM4
@@ -42,28 +44,28 @@ ccsm4h_adj_lst <- qMap(
   wrf_sf[[3]]$sf, 
   ret.deltas = TRUE
 )
-ccsm4f_adj <- qMap(
+sf_lst$ccsm4h <- ccsm4h_adj_lst$df
+sf_lst$ccsm4f <- qMap(
   sim = wrf_sf[[4]]$sf, 
   use.deltas = ccsm4h_adj_lst$deltas
 )
-ccsm4h_adj <- ccsm4h_adj_lst$df
 # lists for ecdfs
 ccsm4h_ecdf_lst <- list(
   obs = era5_sf_adj$sf_adj,
-  sim = ccsm4h_adj$sim, 
-  sim_adj = ccsm4h_adj$sim_adj
+  sim = sf_lst$ccsm4h$sim, 
+  sim_adj = sf_lst$ccsm4h$sim_adj
 )
 ccsm4f_ecdf_lst <- list(
-  obs = ccsm4h_adj$sim_adj,
-  sim = ccsm4f_adj$sim, 
-  sim_adj = ccsm4f_adj$sim_adj
+  obs = sf_lst$ccsm4h$sim_adj,
+  sim = sf_lst$ccsm4f$sim, 
+  sim_adj = sf_lst$ccsm4f$sim_adj
 )
 
 # add dates both models
-cm3h_adj$date <- wrf_sf[[1]]$date
-cm3f_adj$date <- wrf_sf[[2]]$date
-ccsm4h_adj$date <- wrf_sf[[3]]$date
-ccsm4f_adj$date <- wrf_sf[[4]]$date
+sf_lst$cm3h$date <- wrf_sf[[1]]$date
+sf_lst$cm3f$date <- wrf_sf[[2]]$date
+sf_lst$ccsm4h$date <- wrf_sf[[3]]$date
+sf_lst$ccsm4f$date <- wrf_sf[[4]]$date
 
 # plot/save ECDFs
 p1 <- ggECDF_compare(
@@ -100,16 +102,7 @@ ggsave(ecdf_fns[3], p3, width = 7, height = 4.5)
 ggsave(ecdf_fns[4], p4, width = 7, height = 4.5)
 
 # save adjusted data
-adj_fns <- c(
-  "../Nome_Mets_aux/data/cm3h_sf_adj.Rds",
-  "../Nome_Mets_aux/data/cm3f_sf_adj.Rds",
-  "../Nome_Mets_aux/data/ccsm4h_sf_adj.Rds",
-  "../Nome_Mets_aux/data/ccsm4f_sf_adj.Rds"
-)
-
-saveRDS(cm3h_adj, adj_fns[1])
-saveRDS(cm3f_adj, adj_fns[2])
-saveRDS(ccsm4h_adj, adj_fns[3])
-saveRDS(ccsm4f_adj, adj_fns[4])
+fn <- "../Nome_Mets_aux/data/gcm_sf_adj.Rds"
+saveRDS(sf_lst, fn)
 
 #------------------------------------------------------------------------------
--- qmap_gcm_tmin.R
+++ qmap_gcm_tmin.R
@@ -48,26 +48,28 @@ era5_tmin_adj <- readRDS(fn) %>%
 source("helpers.R")
 
 # perform quantile mapping
+# list to store results
+tmin_lst <- list()
 # CM3
 cm3h_adj_lst <- qMap(
   era5_tmin_adj$tmin_adj, wrf_t2min[[1]]$t2min, 
   ret.deltas = TRUE
 )
-cm3f_adj <- qMap(
+tmin_lst$cm3h <- cm3h_adj_lst$df
+tmin_lst$cm3f <- qMap(
   sim = wrf_t2min[[2]]$t2min, 
   use.deltas = cm3h_adj_lst$deltas
 )
-cm3h_adj <- cm3h_adj_lst$df
 # lists for ecdf creation
 cm3h_ecdf_lst <- list(
   obs = era5_tmin_adj$tmin_adj,
-  sim = cm3h_adj$sim, 
-  sim_adj = cm3h_adj$sim_adj
+  sim = tmin_lst$cm3h$sim, 
+  sim_adj = tmin_lst$cm3h$sim_adj
 )
 cm3f_ecdf_lst <- list(
-  obs = cm3h_adj$sim_adj,
-  sim = cm3f_adj$sim, 
-  sim_adj = cm3f_adj$sim_adj
+  obs = tmin_lst$cm3h$sim_adj,
+  sim = tmin_lst$cm3f$sim, 
+  sim_adj = tmin_lst$cm3f$sim_adj
 )
 
 # CCSM4
@@ -77,28 +79,28 @@ ccsm4h_adj_lst <- qMap(
   wrf_t2min[[3]]$t2min, 
   ret.deltas = TRUE
 )
-ccsm4f_adj <- qMap(
+tmin_lst$ccsm4h <- ccsm4h_adj_lst$df
+tmin_lst$ccsm4f <- qMap(
   sim = wrf_t2min[[4]]$t2min, 
   use.deltas = ccsm4h_adj_lst$deltas
 )
-ccsm4h_adj <- ccsm4h_adj_lst$df
 # lists for ecdfs
 ccsm4h_ecdf_lst <- list(
   obs = era5_tmin_adj$tmin_adj,
-  sim = ccsm4h_adj$sim, 
-  sim_adj = ccsm4h_adj$sim_adj
+  sim = tmin_lst$ccsm4h$sim, 
+  sim_adj = tmin_lst$ccsm4h$sim_adj
 )
 ccsm4f_ecdf_lst <- list(
-  obs = ccsm4h_adj$sim_adj,
-  sim = ccsm4f_adj$sim, 
-  sim_adj = ccsm4f_adj$sim_adj
+  obs = tmin_lst$ccsm4h$sim_adj,
+  sim = tmin_lst$ccsm4f$sim, 
+  sim_adj = tmin_lst$ccsm4f$sim_adj
 )
 
 # add dates both models
-cm3h_adj$date <- as.Date(rownames(wrf_t2min[[1]]))
-cm3f_adj$date <- as.Date(rownames(wrf_t2min[[2]]))
-ccsm4h_adj$date <- as.Date(rownames(wrf_t2min[[3]]))
-ccsm4f_adj$date <- as.Date(rownames(wrf_t2min[[4]]))
+tmin_lst$cm3h$date <- as.Date(rownames(wrf_t2min[[1]]))
+tmin_lst$cm3f$date <- as.Date(rownames(wrf_t2min[[2]]))
+tmin_lst$ccsm4h$date <- as.Date(rownames(wrf_t2min[[3]]))
+tmin_lst$ccsm4f$date <- as.Date(rownames(wrf_t2min[[4]]))
 
 # plot/save ECDFs
 p1 <- ggECDF_compare(
@@ -131,16 +133,8 @@ ggsave(ecdf_fns[3], p3, width = 7, height = 4.5)
 ggsave(ecdf_fns[4], p4, width = 7, height = 4.5)
 
 # save adjusted data
-adj_fns <- c(
-  "../Nome_Mets_aux/data/cm3h_t2min_adj.Rds",
-  "../Nome_Mets_aux/data/cm3f_t2min_adj.Rds",
-  "../Nome_Mets_aux/data/ccsm4h_t2min_adj.Rds",
-  "../Nome_Mets_aux/data/ccsm4f_t2min_adj.Rds"
-)
 
-saveRDS(cm3h_adj, adj_fns[1])
-saveRDS(cm3f_adj, adj_fns[2])
-saveRDS(ccsm4h_adj, adj_fns[3])
-saveRDS(ccsm4f_adj, adj_fns[4])
+fn <- "../Nome_Mets_aux/data/gcm_t2min_adj.Rds"
+saveRDS(tmin_lst, fn)
 
 #------------------------------------------------------------------------------
